48da1d8ff0cf7a005d7cdac363e63be7b62a4a7d
Li Hao <353027614@qq.com>
2017-11-18 16:14:27 +0800

    .

--- src/Download.vue
+++ src/Download.vue
@@ -1,16 +1,18 @@
 <template>
-<div class="download"></div>
+  <div class="download"></div>
 </template>
 
 <script>
-export default {
-  name: "download",
-  components: {}
-};
+  export default {
+    name: "download",
+    components: {}
+  };
+
 </script>
 
 <style lang="scss" scoped>
-.download {
-  min-height: 700px;
-}
+  .download {
+    min-height: 700px;
+  }
+
 </style>
