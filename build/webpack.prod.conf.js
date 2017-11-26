8b6de76e14c299873c5fc98732750d09ec367254
maybeeee <lihao19900829@qq.com>
2017-11-26 16:24:56 +0800

    撤销复制static目录

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 const config = require('../config')
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.conf')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+//const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
@@ -88,13 +88,13 @@ const webpackConfig = merge(baseWebpackConfig, {
       chunks: ['vendor']
     }),
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
+
+    //new CopyWebpackPlugin([
+    //{
+    //  from: path.resolve(__dirname, '../src/assets'),
+    //  to: config.build.assetsSubDirectory,
+    //  ignore: ['.*']
+    //}])
   ]
 })
 
